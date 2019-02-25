0f84334c6517eacd1abafbe48603ba7d7bb484d4
Ming Duan <11203duan@gmail.com>
2019-02-25 19:26:11 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if collection.class == Fixnum
+    if collection < 3
+      "true"
+    else "false"
+    end
+  else
+    i = 0
+  arr = []
+  while i < collection.length
+    arr << yield(collection[i])
+    i += 1
+  end
+  if arr.include?(false)
+     false
+  else true
 end
+end
+end
+
+my_all?(2) { 2 < 3 }
