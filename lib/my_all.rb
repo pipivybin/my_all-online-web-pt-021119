01cc5d9143a3e9313564e2ab48634cfdd981ebe7
Ming Duan <11203duan@gmail.com>
2019-02-25 19:21:45 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+  binding.pry
+    i = 0
+  arr = []
+  while i < collection.length - 1
+    arr << yield(collection[i])
+    i += 1
+  end
+  if arr.include?("false")
+     "false"
+  else "true"
 end
+end
+
+my_all?(2) { 2 < 3 }
